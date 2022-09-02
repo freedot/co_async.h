365e4852ae922475e605344815958412381977e2
qujianbiao <qujianbiao@users.noreply.github.com>
2022-09-02 11:41:17 +0800

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,31 +1,36 @@
+// 示例一
 #include "co_async.h"
 #include <iostream>
 
-
-std::function<void(int)> g_resolve_cb;
+std::function<void(int)> resolve_cb;
 
 co::async<int> get_id() {
-  co_return 1;
+  // 等待一个承若协程
+  int r = co_await co::promise<int>([](auto resolve_cb) {
+    ::resolve_cb = std::move(resolve_cb);
+    });
+  co_return r;
 }
 
-co::async<int> fun() {
-  auto id = co_await get_id();
-
-  auto w = co_await co::promise<int>([](auto resolve_cb) {
-    g_resolve_cb = std::move(resolve_cb);
-    });
-
-  co_return id * 10 + w;
+co::async<std::string> fun() {
+  int id = co_await get_id();
+  co_return std::format("id: {}", id);
 }
 
 int main() {
+  auto result = fun().await([]() {
 
-  int result = fun().await([]() {
-    g_resolve_cb(2);
+    // 当所有协程被挂起后，会执行该update函数，该函数在主线程中执行
+    // 当该update返回false，则会强制结束await等待，有可能抛出异常
+    // 在这里可以做恢复协程操作
+
+    resolve_cb(1); // 恢复协程, id值为1
+ 
     return true;
+
     });
-  std::cout << "result: " << result << std::endl;
+
+  std::cout << result << std::endl;
 
   return 0;
 }
-
